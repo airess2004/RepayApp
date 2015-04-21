c8d3bb3b5fa88d481cb2a921be47ac3345e79b5c
ANR2ME <anr2000@gmail.com>
2015-04-21 13:58:11 +0700

    Added sliding page

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,93 @@
 //var fa = Alloy.createWidget("com.mattmcfarland.fontawesome");
 //win.add(fa);
 
+Alloy.Globals.login = Alloy.createController("login");
+
 function doClick(e) {
     alert($.label.text);
 }
 
-function doLoginClick(e) {
-    var loginView = Alloy.createController("login").getView().open();
-}
-
-function buttonBarClick(e) {
-	var reimburse = Alloy.createController('reimburse').getView();
-	reimburse.open();
-	
-}
-
-
-function mainViewOpen(e) {
-	if (!Alloy.Globals.CURRENT_USER || Alloy.Globals.CURRENT_USER=="") {
-		var loginView = Alloy.createController("login").getView().open();
+function doMenuClick(evt) {
+  switch(evt.source.title){
+		case "Menu": // in real life you probably wouldn't want to use the text of the menu option as your condition
+			var activity = $.index.getActivity();
+			activity.openOptionsMenu();
+			break;
+		default:
+			alert(evt.source.title);	
 	}
 }
+
+function doSearch(e) {
+	alert("Search Clicked");
+}
+
+function doLoginClick(e) {
+    Alloy.Globals.login.getView().open();
+}
+
+function homeBtnClick(e) {
+	var page = 0;
+	$.scrollableView.scrollToView(page);
+	//$.scrollableView.setCurrentPage(page);
+	$.scrollableView.views[page].fireEvent("open");
+	var actionBar = $.index.getActivity().getActionBar();
+	actionBar.title = 'Home';
+}
+
+function reimburseBtnClick(e) {
+	var page = 1;
+	$.scrollableView.scrollToView(page);
+	//$.scrollableView.setCurrentPage(page);
+	$.scrollableView.views[page].fireEvent("open");
+	var actionBar = $.index.getActivity().getActionBar();
+	actionBar.title = 'Reimburse';
+}
+
+function settingBtnClick(e) {
+	var page = 2;
+	$.scrollableView.scrollToView(page);
+	//$.scrollableView.setCurrentPage(page);
+	$.scrollableView.views[page].fireEvent("open");
+	var actionBar = $.index.getActivity().getActionBar();
+	actionBar.title = 'Setting';
+}
+
+function mainViewOpen(e) {
+	Alloy.Globals.index = $.index;
+	//Alloy.Globals.abx = require('com.alcoapps.actionbarextras');
+	var actionBar = $.index.getActivity().getActionBar();
+	// get a handle to the action bar
+	actionBar.title = 'RepayApp';
+	// change the App Title
+	//actionBar.displayHomeAsUp = true; // back icon
+	// Show the "angle" pointing back
+	actionBar.onHomeIconItemSelected = function() {// what to do when the "home" icon is pressed
+		Ti.API.info("Home icon clicked!");
+	};
+	// Make sure icons are updated
+	$.index.activity.invalidateOptionsMenu();
+	
+	if (!Alloy.Globals.CURRENT_USER || Alloy.Globals.CURRENT_USER=="") {
+		Alloy.Globals.CURRENT_USER = "Admin"; 
+		//Alloy.Globals.login.getView().open();
+	}
+}
+
+function scrollableViewScrollEnd(e) {
+	e.view.fireEvent("open");
+}
+
+// $.index.addEventListener('open', function() {
+	// var actionBar = $.index.getActivity().getActionBar();
+	// // get a handle to the action bar
+	// actionBar.title = 'RepayApp';
+	// // change the App Title
+	// actionBar.displayHomeAsUp = true;
+	// // Show the "angle" pointing back
+	// actionBar.onHomeIconItemSelected = function() {// what to do when the "home" icon is pressed
+		// Ti.API.info("Home icon clicked!");
+	// };
+// });
 
 $.index.open();
