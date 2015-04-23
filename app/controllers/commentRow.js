ed124eb3425aeddc43aae9da165912686c1d392f
johan <airess2004@yahoo.com>
2015-04-23 21:22:42 +0700

    ase

---
+++ app/controllers/commentRow.js
@@ -0,0 +1,21 @@
+var args = arguments[0] || {};
+
+var moment = require('alloy/moment');
+var comments = Alloy.Collections.comment;
+var id;
+
+// $model represents the current model accessible to this
+// controller from the markup's model-view binding. $model
+// will be null if there is no binding in place.
+
+if ($model) {
+	id = $model.id;
+	$.commentRow.rowid = $model.id;
+}
+
+
+function thumbPopUp(e) {
+	
+}
+
+
